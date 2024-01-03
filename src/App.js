58ac5b29e8d174a1894b36e9e5154391e2fa45ac
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-01-04 00:18:52 +0100

    chore: add memo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, memo, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { usePosts, PostProvider } from "./PostContext";
 import Test from "./Test";
@@ -77,14 +77,14 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main = memo(function Main() {
   return (
     <main>
       <FormAddPost />
       <Posts />
     </main>
   );
-}
+});
 
 function Posts() {
   return (
